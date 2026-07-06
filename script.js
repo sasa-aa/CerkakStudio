bf139ff518f19581557f45e63583906ad7cbe5b4
sasa-aa <sasa.ayu.anissa@gmail.com>
2026-07-06 08:16:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const startButton =
+document.getElementById("startButton");
+
+startButton.addEventListener("click",()=>{
+
+alert(
+"Selamat datang di Cerkak Studio!\n\nPada bagian berikutnya kita akan mulai mengisi identitas siswa."
+);
+
+});
